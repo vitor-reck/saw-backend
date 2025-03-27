844541dab5307da24c8b42462bb56dfd8bae33bb
vitor-reck <vitor_reck.150@hotmail.com>
2025-03-27 15:41:42 -0300

    Logging services and remove catching exceptions on jwt

--- app/src/main/java/br/vitorreck/app/services/CategoryService.java
+++ app/src/main/java/br/vitorreck/app/services/CategoryService.java
@@ -5,6 +5,7 @@ import br.vitorreck.app.domain.model.Category;
 import br.vitorreck.app.mappers.CategoryMapper;
 import br.vitorreck.app.repositories.CategoryRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,6 +13,7 @@ import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
+@Log4j2
 public class CategoryService {
 
   private static final String CATEGORY_NOT_FOUND = "Category doesn't exist";
@@ -31,6 +33,7 @@ public class CategoryService {
   }
 
   public Category retrieveCategoryByName(String name) {
+    log.info("POST /products - retrieving category with name: {}", name);
     return categoryRepository.findByName(name)
         .orElseThrow(() -> new NoSuchElementException(CATEGORY_NOT_FOUND));
   }
